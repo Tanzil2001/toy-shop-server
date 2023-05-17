e3b90236a68a20a50fe74705a89bf6646cfa6c56
Mahin Jasim <barakattanzil@gmail.comm>
2023-05-17 23:15:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000 ;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res)=>{
+    res.send('mera bhi ayega')
+})
+
+app.listen(port, ()=>{
+    console.log(`running on port ${port}`);
+})
